a8c6533c1622eba21c03cac9f20e3d35c53ac60a
aurnor <33629764+aurnor@users.noreply.github.com>
2025-05-22 09:10:03 +0200

    New test

--- Program.cs
+++ Program.cs
@@ -1,11 +1,9 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
 app.UseHttpsRedirection();
 
 var summaries = new[]
